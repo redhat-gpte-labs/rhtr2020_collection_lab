e3bc5ca13af8eb22ef2e7e213db5afcdf5392196
Tony <tony.g.kay@gmail.com>
2020-10-29 10:15:25 -0600

    Added documentation to the my_module

--- resources/my_module.py
+++ resources/my_module.py
@@ -5,6 +5,43 @@ __metaclass__ = type
 
 from ansible.module_utils.basic import AnsibleModule
 
+DOCUMENTATION = '''
+---
+module: my_module
+version_added: historical
+short_description: TOutput a string
+description:
+   - A trivial test module, this looks like it was based on ping
+   - ssh or local connections only
+options:
+  data:
+    description:
+      - Data to return for the C(ping) return value.
+      - If this parameter is set to C(crash), the module will cause an exception.
+    type: str
+    default: Hello RHTR!  This is my new module, borrowed from somewhere!
+author:
+    - Unknown
+    - Michael DeHaan?
+'''
+
+EXAMPLES = '''
+# Test we can logon to 'webservers' and execute python with json lib.
+# ansible webservers -m my_module
+- name: Example from an Ansible Playbook
+  my_module:
+- name: Induce an exception to see what happens
+  my_module:
+    data: crash
+'''
+
+RETURN = '''
+my_module:
+    description: value provided with the data parameter
+    returned: success
+    type: str
+    sample: Hello RHTR!  This is my new module, borrowed from somewhere!
+'''
 
 def main():
     module = AnsibleModule(
